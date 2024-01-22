b9717046768099d4ad98aa0337915437593f88f0
Matan <matanatedgi@gmail.com>
2024-01-22 17:18:59 +0200

    Add icon image, Create MainApplication class, Create main.py

---
+++ extensions/main_application.py
@@ -0,0 +1,70 @@
+# Standard library imports
+import os
+
+# Third-party library imports
+from customtkinter import CTk, set_appearance_mode
+
+# Local imports
+from views import MainView
+
+
+class MainApplication(CTk):
+    """
+    MainApplication class represents the main application window.
+
+    This class extends Tk and serves as the main application window. It contains methods for initializing
+    the main view, handling the switching of views, and running the main application loop.
+
+    Parameters:
+        *args, **kwargs: Additional arguments passed to the Tk constructor.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the MainApplication.
+
+        Parameters:
+        *args, **kwargs: Additional arguments passed to the Tk constructor.
+        """
+        super().__init__(*args, **kwargs)
+
+        # Initialize the appearance mode to "system"
+        set_appearance_mode("system")
+
+        self.current_view = None
+
+        # Initialize Window
+        self.title("Typing Speed Test")
+        self.iconbitmap(os.path.join("resources", "images", "logo.ico"))
+
+        # Calculate the window dimensions to 80% of the screen
+        self.window_w = int(self.winfo_screenwidth() * 0.8)
+        self.window_h = int(self.winfo_screenheight() * 0.8)
+
+        # Calculate the position of the screen so it will be centered
+        self.x = (self.winfo_screenwidth() // 2) - (self.window_w // 2)
+        self.y = (self.winfo_screenheight() // 2) - (self.window_h // 2)
+
+        # Set the window geometry
+        self.geometry(f"{self.window_w}x{self.window_h}+{self.x}+{self.y}")
+
+        self.switch_to_main_view()
+
+    def switch_view(self, view_class, *args, **kwargs):
+        """Switches to a new view and destroys the current one.
+
+        Args:
+            view_class (class): The class of the view to switch to.
+            *args: Additional positional arguments for the new view.
+            **kwargs: Additional keyword arguments for the new view.
+        """
+        selected_view = view_class(self, self.switch_view, *args, **kwargs)
+
+        if self.current_view:
+            self.current_view.destroy()
+
+        selected_view.pack(fill="both", expand=True)
+        self.current_view = selected_view
+
+    def switch_to_main_view(self):
+        self.after(50, lambda: self.switch_view(MainView))
---
+++ main.py
@@ -0,0 +1,17 @@
+# Standard library imports
+
+
+# Third-party library imports
+
+
+# Local imports
+from extensions import MainApplication
+
+
+def main():
+    root = MainApplication()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
